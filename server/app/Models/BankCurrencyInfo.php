7852efef727551471ce104269080d83dc9e38553
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-03-10 16:44:57 +0300

    add parser rework

--- server/app/Models/BankCurrencyInfo.php
+++ server/app/Models/BankCurrencyInfo.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\{
     Factories\HasFactory,
-    Casts\Attribute,
     Model
 };
 
 class BankCurrencyInfo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['*'];
 
     protected $casts = [
         'phones' => 'array',
