726a60ce2166258e2bc4507c840e4d47a8552796
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-03-11 16:15:55 +0300

    remove unused api route code

--- server/routes/api.php
+++ server/routes/api.php
@@ -14,7 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/get/nearest/departments', [ApiController::class, 'getNearestDepartments']);
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
