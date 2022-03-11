8c09d304e6a887952952969bf29ed6fe0bb09bf9
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-03-11 12:28:13 +0300

    remove last update cast to date

--- server/app/Models/BankCurrencyInfo.php
+++ server/app/Models/BankCurrencyInfo.php
@@ -18,7 +18,6 @@ class BankCurrencyInfo extends Model
         'coordinates' => 'array',
         'currency_info' => 'array',
         'created_at' => 'datetime:Y-m-d H:i:s',
-        'updated_at' => 'datetime:Y-m-d H:i:s',
-        'last_update' => 'datetime:Y-m-d H:i:s'
+        'updated_at' => 'datetime:Y-m-d H:i:s'
     ];
 }
